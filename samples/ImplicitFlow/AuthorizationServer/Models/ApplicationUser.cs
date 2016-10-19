5d466c53964aeaf0f743540e6153522a29db9c1c
Kévin Chalet <kevinchalet@gmail.com>
2016-10-19 21:24:00 +0200

    React to API changes in openiddict/openiddict-core
    https://github.com/openiddict/openiddict-core/commit/bc5f21ad26714fc486de73c9df6634aaff0a038d

--- samples/ImplicitFlow/AuthorizationServer/Models/ApplicationUser.cs
+++ samples/ImplicitFlow/AuthorizationServer/Models/ApplicationUser.cs
@@ -1,6 +1,6 @@
-﻿using OpenIddict;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace AuthorizationServer.Models {
     // Add profile data for application users by adding properties to the ApplicationUser class
-    public class ApplicationUser : OpenIddictUser { }
+    public class ApplicationUser : IdentityUser { }
 }
