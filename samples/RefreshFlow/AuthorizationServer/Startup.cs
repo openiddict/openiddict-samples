81720b4954a027cc7ae2a902080ce1da27f46e83
Fabian Wiles <fabianwiles@live.com>
2016-12-02 13:15:32 +1300

    removed uneeded constructor and property

--- samples/RefreshFlow/AuthorizationServer/Startup.cs
+++ samples/RefreshFlow/AuthorizationServer/Startup.cs
@@ -11,20 +11,6 @@ namespace AuthorizationServer
 {
     public class Startup
     {
-
-        public Startup(IHostingEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-
-            Configuration = builder.Build();
-        }
-
-        public IConfigurationRoot Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
