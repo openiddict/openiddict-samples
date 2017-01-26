470e8d241bd7ee86a87eab51b32ec607ff85d32f
Kévin Chalet <kevinchalet@gmail.com>
2017-01-26 18:38:56 +0100

    Remove AddEphemeralSigningKey() calls from the code/client credentials/password/refresh token samples

--- samples/ClientCredentialsFlow/AuthorizationServer/Startup.cs
+++ samples/ClientCredentialsFlow/AuthorizationServer/Startup.cs
@@ -40,28 +40,7 @@ namespace AuthorizationServer {
                 .AllowClientCredentialsFlow()
 
                 // During development, you can disable the HTTPS requirement.
-                .DisableHttpsRequirement()
-
-                // Register a new ephemeral key, that is discarded when the application
-                // shuts down. Tokens signed using this key are automatically invalidated.
-                // This method should only be used during development.
-                .AddEphemeralSigningKey();
-
-            // On production, using a X.509 certificate stored in the machine store is recommended.
-            // You can generate a self-signed certificate using Pluralsight's self-cert utility:
-            // https://s3.amazonaws.com/pluralsight-free/keith-brown/samples/SelfCert.zip
-            //
-            // services.AddOpenIddict()
-            //     .AddSigningCertificate("7D2A741FE34CC2C7369237A5F2078988E17A6A75");
-            //
-            // Alternatively, you can also store the certificate as an embedded .pfx resource
-            // directly in this assembly or in a file published alongside this project:
-            //
-            // services.AddOpenIddict()
-            //     .AddSigningCertificate(
-            //          assembly: typeof(Startup).GetTypeInfo().Assembly,
-            //          resource: "AuthorizationServer.Certificate.pfx",
-            //          password: "OpenIddict");
+                .DisableHttpsRequirement();
         }
 
         public void Configure(IApplicationBuilder app) {
--- samples/RefreshFlow/AuthorizationServer/Startup.cs
+++ samples/RefreshFlow/AuthorizationServer/Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace AuthorizationServer {
     public class Startup {
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddCors();
             services.AddMvc();
@@ -44,28 +43,7 @@ namespace AuthorizationServer {
                 .AllowRefreshTokenFlow()
 
                 // During development, you can disable the HTTPS requirement.
-                .DisableHttpsRequirement()
-
-                // Register a new ephemeral key, that is discarded when the application
-                // shuts down. Tokens signed using this key are automatically invalidated.
-                // This method should only be used during development.
-                .AddEphemeralSigningKey();
-
-            // On production, using a X.509 certificate stored in the machine store is recommended.
-            // You can generate a self-signed certificate using Pluralsight's self-cert utility:
-            // https://s3.amazonaws.com/pluralsight-free/keith-brown/samples/SelfCert.zip
-            //
-            // services.AddOpenIddict()
-            //     .AddSigningCertificate("7D2A741FE34CC2C7369237A5F2078988E17A6A75");
-            //
-            // Alternatively, you can also store the certificate as an embedded .pfx resource
-            // directly in this assembly or in a file published alongside this project:
-            //
-            // services.AddOpenIddict()
-            //     .AddSigningCertificate(
-            //          assembly: typeof(Startup).GetTypeInfo().Assembly,
-            //          resource: "AuthorizationServer.Certificate.pfx",
-            //          password: "OpenIddict");
+                .DisableHttpsRequirement();
         }
 
         public void Configure(IApplicationBuilder app) {
