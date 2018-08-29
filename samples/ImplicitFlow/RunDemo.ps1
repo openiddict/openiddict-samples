8a5dca28d6bd1463e6961137565375267ff438ca
Shaun Luttin <shaunluttin@gmail.com>
2018-08-29 15:21:14 -0700

    Update RunDemo.ps1 script

--- samples/ImplicitFlow/RunDemo.ps1
+++ samples/ImplicitFlow/RunDemo.ps1
@@ -1,24 +1,10 @@
-$global:p = @();
-
-function global:Find-ChildProcess {
-  param($ID=$PID)
-
-  $result = Get-CimInstance win32_process |
-    Where-Object { $_.ParentProcessId -eq $ID }
-    Select-Object -Property ProcessId
-
-  $result
-  $result |
-    Where-Object { $_.ProcessId -ne $null } |
-    ForEach-Object {
-      Find-ChildProcess -id $_.ProcessId
-    }
-}
-
 function global:Kill-Demo {
-  $Global:p |
-    ForEach-Object { Find-ChildProcess -ID $_.Id } |
-    ForEach-Object { Stop-Process -id $_.ProcessId }
+    Write-Host "Kill all dotnet and node processes (default no)?";
+    $response = Read-Host "Input 'y' for yes; press enter to continue" 
+    if ('y' -eq $response) {
+       kill -name node -ErrorAction SilentlyContinue;
+       kill -name dotnet -ErrorAction SilentlyContinue; 
+    }
 }
 
 [System.Environment]::SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development");
@@ -27,26 +13,28 @@ function global:Kill-Demo {
 Push-Location "./AuthorizationServer"
 dotnet restore
 dotnet build --no-incremental #rebuild
-$global:p += Start-Process dotnet -ArgumentList "watch run server.urls=http://localhost:12345" -PassThru
+Start-Process dotnet -ArgumentList "watch run server.urls=http://localhost:12345" -PassThru 
 Pop-Location
+
+exit
 
 # Aurelia Application
 Push-Location "./AureliaApp"
 npm install -y
-$global:p += Start-Process yarn -ArgumentList "run start" -PassThru
+Start-Process yarn -ArgumentList "run start" -PassThru
 Pop-Location
 
 # Resource Server 01
 Push-Location "./ResourceServer01"
 dotnet restore
 dotnet build --no-incremental
-$global:p += Start-Process dotnet -ArgumentList "watch run server.urls=http://localhost:5001" -PassThru
+Start-Process dotnet -ArgumentList "watch run server.urls=http://localhost:5001" -PassThru
 Pop-Location
 
 # Resource Server 02
 Push-Location "./ResourceServer02"
 dotnet restore
 dotnet build --no-incremental
-$global:p += Start-Process dotnet -ArgumentList "watch run server.urls=http://localhost:5002" -PassThru
+Start-Process dotnet -ArgumentList "watch run server.urls=http://localhost:5002" -PassThru
 Pop-Location
 
