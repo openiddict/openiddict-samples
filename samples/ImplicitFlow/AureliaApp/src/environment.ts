08b6166ae03804d99bb4da635558d0fde88dd4f5
Shaun Luttin <shaunluttin@gmail.com>
2019-03-18 15:27:54 -0700

    Align client config with auth server config

--- samples/ImplicitFlow/AureliaApp/src/environment.ts
+++ samples/ImplicitFlow/AureliaApp/src/environment.ts
@@ -2,7 +2,7 @@ export default {
   debug: true,
   testing: true,
   urls: {
-    authority: "http://localhost:5000",
+    authority: "http://localhost:12345",
     host: "http://localhost:9000",
     resourceServer01: "http://localhost:5001",
     resourceServer02: "http://localhost:5002",
