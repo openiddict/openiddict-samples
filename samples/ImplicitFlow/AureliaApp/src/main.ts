a838fe502ee2aad61a4b47603f9d4757e6f93f67
Shaun Luttin <shaunluttin@gmail.com>
2019-03-20 09:28:42 -0700

    Delete commented out code

--- samples/ImplicitFlow/AureliaApp/src/main.ts
+++ samples/ImplicitFlow/AureliaApp/src/main.ts
@@ -1,16 +1,12 @@
 import { Aurelia } from 'aurelia-framework'
 import environment from './environment';
 import oidcConfig from "./open-id-connect-configuration";
-// import oidcConfig from "./open-id-connect-configuration-auth0";
-// import oidcConfig from "./open-id-connect-configuration-azure";
-// import oidcConfig from "./open-id-connect-configuration-identity-server";
 
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
     .feature('resources');
 
-  // Simplified configuration as of v0.19.0.
   aurelia.use.plugin("aurelia-open-id-connect", () => oidcConfig);
 
   if (environment.debug) {
