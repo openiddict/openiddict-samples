62cb03bda5f2ca5fae56b8525f4176177f856753
IgorHrabrov <18480638+IgorHrabrov@users.noreply.github.com>
2019-10-13 13:51:28 +0300

    Update AuthorizationController.cs to use the correct roles scope constant

--- samples/RefreshFlow/AuthorizationServer/Controllers/AuthorizationController.cs
+++ samples/RefreshFlow/AuthorizationServer/Controllers/AuthorizationController.cs
@@ -161,7 +161,7 @@ namespace AuthorizationServer.Controllers
                 // The other claims will only be added to the access_token, which is encrypted when using the default format.
                 if ((claim.Type == OpenIdConnectConstants.Claims.Name && ticket.HasScope(OpenIdConnectConstants.Scopes.Profile)) ||
                     (claim.Type == OpenIdConnectConstants.Claims.Email && ticket.HasScope(OpenIdConnectConstants.Scopes.Email)) ||
-                    (claim.Type == OpenIdConnectConstants.Claims.Role && ticket.HasScope(OpenIddictConstants.Claims.Roles)))
+                    (claim.Type == OpenIdConnectConstants.Claims.Role && ticket.HasScope(OpenIddictConstants.Scopes.Roles)))
                 {
                     destinations.Add(OpenIdConnectConstants.Destinations.IdentityToken);
                 }
