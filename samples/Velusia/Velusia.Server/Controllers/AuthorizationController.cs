90bbb8c872bf59f8e84651b10b0fb3fbb6f38554
Kévin Chalet <kevinchalet@gmail.com>
2020-06-16 02:49:50 +0200

    Don't set the AuthenticationProperties.RedirectUri fallback address from LogoutPost to work around a beta1 bug

--- samples/Velusia/Velusia.Server/Controllers/AuthorizationController.cs
+++ samples/Velusia/Velusia.Server/Controllers/AuthorizationController.cs
@@ -306,14 +306,8 @@ namespace Velusia.Server.Controllers
             await _signInManager.SignOutAsync();
 
             // Returning a SignOutResult will ask OpenIddict to redirect the user agent
-            // to the post_logout_redirect_uri specified by the client application or to
-            // the RedirectUri specified in the authentication properties if none was set.
-            return SignOut(
-                authenticationSchemes: OpenIddictServerAspNetCoreDefaults.AuthenticationScheme,
-                properties: new AuthenticationProperties
-                {
-                    RedirectUri = "/"
-                });
+            // to the post_logout_redirect_uri specified by the client application.
+            return SignOut(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
         }
 
         [HttpPost("~/connect/token"), Produces("application/json")]
