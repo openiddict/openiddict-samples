5b9621ebfc1f5c72a561f83e0543b0d0ad007d53
Kévin Chalet <kevinchalet@gmail.com>
2021-02-09 15:38:22 +0100

    Add a Windows Integrated Authentication sample

---
+++ samples/Weytta/Weytta.Server/Controllers/AuthorizationController.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/openiddict/openiddict-core for more information concerning
+ * the license and the contributors participating to this project.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Negotiate;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using OpenIddict.Abstractions;
+using OpenIddict.Server.AspNetCore;
+using static OpenIddict.Abstractions.OpenIddictConstants;
+
+namespace Velusia.Server.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        private readonly IOpenIddictApplicationManager _applicationManager;
+
+        public AuthorizationController(IOpenIddictApplicationManager applicationManager)
+            => _applicationManager = applicationManager;
+
+        [HttpGet("~/connect/authorize")]
+        [HttpPost("~/connect/authorize")]
+        [IgnoreAntiforgeryToken]
+        public async Task<IActionResult> Authorize()
+        {
+            var request = HttpContext.GetOpenIddictServerRequest() ??
+                throw new InvalidOperationException("The OpenID Connect request cannot be retrieved.");
+
+            // Retrieve the Windows identity associated with the current authorization request.
+            // If it can't be extracted, trigger a Integrated Windows Authentication dance.
+            var result = await HttpContext.AuthenticateAsync(NegotiateDefaults.AuthenticationScheme);
+            if (result is not { Succeeded: true })
+            {
+                return Challenge(
+                    authenticationSchemes: NegotiateDefaults.AuthenticationScheme,
+                    properties: new AuthenticationProperties
+                    {
+                        RedirectUri = Request.PathBase + Request.Path + QueryString.Create(
+                            Request.HasFormContentType ? Request.Form.ToList() : Request.Query.ToList())
+                    });
+            }
+
+            // Retrieve the application details from the database.
+            var application = await _applicationManager.FindByClientIdAsync(request.ClientId) ??
+                throw new InvalidOperationException("Details concerning the calling client application cannot be found.");
+
+            // This sample doesn't include a consent view mechanism and requires that the application use implicit consents.
+            if (!await _applicationManager.HasConsentTypeAsync(application, ConsentTypes.Implicit))
+            {
+                return Forbid(
+                    authenticationSchemes: OpenIddictServerAspNetCoreDefaults.AuthenticationScheme,
+                    properties: new AuthenticationProperties(new Dictionary<string, string>
+                    {
+                        [OpenIddictServerAspNetCoreConstants.Properties.Error] = Errors.ServerError,
+                        [OpenIddictServerAspNetCoreConstants.Properties.ErrorDescription] =
+                            "The specified client application is not correctly configured."
+                    }));
+            }
+
+            // The Windows identity doesn't contain the "sub" claim required by OpenIddict to represent
+            // a stable identifier of the authenticated user. To work around that, a "sub" claim is
+            // manually created by using the primary SID claim resolved from the Windows identity.
+            var identity = new ClaimsIdentity(result.Principal.Claims, TokenValidationParameters.DefaultAuthenticationType);
+
+            var sid = identity.FindFirst(ClaimTypes.PrimarySid)?.Value;
+            identity.AddClaim(new Claim(Claims.Subject, sid));
+
+            // Allow all the claims resolved from the principal to be copied to the access and identity tokens.
+            foreach (var claim in identity.Claims)
+            {
+                claim.SetDestinations(Destinations.AccessToken, Destinations.IdentityToken);
+            }
+
+            return SignIn(new ClaimsPrincipal(identity), OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+        }
+    }
+}
---
+++ samples/Weytta/Weytta.Server/Worker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OpenIddict.Abstractions;
+using static OpenIddict.Abstractions.OpenIddictConstants;
+
+namespace Weytta.Server
+{
+    public class Worker : IHostedService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public Worker(IServiceProvider serviceProvider)
+            => _serviceProvider = serviceProvider;
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using var scope = _serviceProvider.CreateScope();
+
+            var context = scope.ServiceProvider.GetRequiredService<DbContext>();
+            await context.Database.EnsureCreatedAsync();
+
+            var manager = scope.ServiceProvider.GetRequiredService<IOpenIddictApplicationManager>();
+
+            if (await manager.FindByClientIdAsync("console_app") == null)
+            {
+                await manager.CreateAsync(new OpenIddictApplicationDescriptor
+                {
+                    ClientId = "console_app",
+                    ConsentType = ConsentTypes.Implicit,
+                    DisplayName = "Console application",
+                    RedirectUris =
+                    {
+                        new Uri("http://localhost:7890/")
+                    },
+                    Permissions =
+                    {
+                        Permissions.Endpoints.Authorization,
+                        Permissions.Endpoints.Token,
+                        Permissions.GrantTypes.AuthorizationCode,
+                        Permissions.ResponseTypes.Code
+                    },
+                    Requirements =
+                    {
+                        Requirements.Features.ProofKeyForCodeExchange
+                    }
+                });
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+}
