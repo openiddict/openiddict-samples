061da8f3e0f44988311a4cbc9fe73624c352a9b2
Mohamed Seada <mohamed.seada.1994@gmail.com>
2021-07-12 18:47:42 +0200

    Set the list of scopes granted to the client application in access_token

--- samples/Aridka/Aridka.Server/Controllers/AuthorizationController.cs
+++ samples/Aridka/Aridka.Server/Controllers/AuthorizationController.cs
@@ -5,8 +5,11 @@
  */
 
 using System;
+using System.Collections.Immutable;
+using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Aridka.Server.Helpers;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
@@ -19,9 +22,13 @@ namespace Aridka.Server.Controllers
     public class AuthorizationController : Controller
     {
         private readonly IOpenIddictApplicationManager _applicationManager;
+        private readonly IOpenIddictScopeManager _scopeManager;
 
-        public AuthorizationController(IOpenIddictApplicationManager applicationManager)
-            => _applicationManager = applicationManager;
+        public AuthorizationController(IOpenIddictApplicationManager applicationManager, IOpenIddictScopeManager scopeManager)
+        {
+            _applicationManager = applicationManager;
+            _scopeManager = scopeManager;
+        }
 
         [HttpPost("~/connect/token"), Produces("application/json")]
         public async Task<IActionResult> Exchange()
@@ -51,10 +58,40 @@ namespace Aridka.Server.Controllers
                 identity.AddClaim(Claims.Name, await _applicationManager.GetDisplayNameAsync(application),
                     Destinations.AccessToken, Destinations.IdentityToken);
 
-                return SignIn(new ClaimsPrincipal(identity), OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+                // Set the list of scopes granted to the client application in access_token.
+                var principal = new ClaimsPrincipal(identity);
+                principal.SetScopes(request.GetScopes());
+                principal.SetResources(await _scopeManager.ListResourcesAsync(principal.GetScopes()).ToListAsync());
+
+                foreach (var claim in principal.Claims)
+                {
+                    claim.SetDestinations(GetDestinations(claim));
+                }
+
+                return SignIn(principal, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
             }
 
             throw new NotImplementedException("The specified grant type is not implemented.");
+        }
+
+        private IEnumerable<string> GetDestinations(Claim claim)
+        {
+            // Note: by default, claims are NOT automatically included in the access and identity tokens.
+            // To allow OpenIddict to serialize them, you must attach them a destination, that specifies
+            // whether they should be included in access tokens, in identity tokens or in both.
+
+            return claim.Type switch
+            {
+                Claims.Name or 
+                Claims.Subject 
+                    => ImmutableArray.Create(Destinations.AccessToken, Destinations.IdentityToken),
+
+                // Never include the security stamp in the access and identity tokens, as it's a secret value.
+                "AspNet.Identity.SecurityStamp" 
+                    => ImmutableArray.Create<string>(),
+
+                _ => ImmutableArray.Create(Destinations.AccessToken),
+            };
         }
     }
 }
