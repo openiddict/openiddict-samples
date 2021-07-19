50e73cb8981b43db691389236c6d93eec9a7b442
Arjun Nakkeeran <arjun.nakkeeran@hm.com>
2021-07-19 15:41:08 +0530

    Add Kalarba Server project using Microsoft.Extensions.DependencyInjection instead of autofac

---
+++ samples/Kalarba/KalarbaMSDI.Server/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace Kalarba.Server
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string address = "http://localhost:58779/";
+            using (WebApp.Start<Startup>(address))
+            {
+                Console.WriteLine($"Server is running on {address}, press CTRL+C to stop.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
