3c8b4b0e1c8d14bcd8c622e48b7530d9fd31a231
Mohamed Seada <mohamed.seada.1994@gmail.com>
2021-07-19 10:37:00 +0200

    Update the Aridka sample to set the list of scopes granted to the client application

--- samples/Aridka/Aridka.Server/Controllers/AuthorizationController.cs
+++ samples/Aridka/Aridka.Server/Controllers/AuthorizationController.cs
@@ -5,8 +5,11 @@
  */
 
 using System;
+using System.Collections.Immutable;
+using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Aridka.Server.Helpers;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
@@ -19,9 +22,13 @@ namespace Aridka.Server.Controllers
     public class AuthorizationController : Controller
     {
         private readonly IOpenIddictApplicationManager _applicationManager;
+        private readonly IOpenIddictScopeManager _scopeManager;
 
-        public AuthorizationController(IOpenIddictApplicationManager applicationManager)
-            => _applicationManager = applicationManager;
+        public AuthorizationController(IOpenIddictApplicationManager applicationManager, IOpenIddictScopeManager scopeManager)
+        {
+            _applicationManager = applicationManager;
+            _scopeManager = scopeManager;
+        }
 
         [HttpPost("~/connect/token"), Produces("application/json")]
         public async Task<IActionResult> Exchange()
@@ -50,11 +57,45 @@ namespace Aridka.Server.Controllers
 
                 identity.AddClaim(Claims.Name, await _applicationManager.GetDisplayNameAsync(application),
                     Destinations.AccessToken, Destinations.IdentityToken);
+                
+                // Note: In the original OAuth 2.0 specification, the client credentials grant
+                // doesn't return an identity token, which is an OpenID Connect concept.
+                //
+                // As a non-standardized extension, OpenIddict allows returning an id_token
+                // to convey information about the client application when the "openid" scope
+                // is granted (i.e specified when calling principal.SetScopes()). When the "openid"
+                // scope is not explicitly set, no identity token is returned to the client application.
 
-                return SignIn(new ClaimsPrincipal(identity), OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+                // Set the list of scopes granted to the client application in access_token.
+                var principal = new ClaimsPrincipal(identity);
+                principal.SetScopes(request.GetScopes());
+                principal.SetResources(await _scopeManager.ListResourcesAsync(principal.GetScopes()).ToListAsync());
+
+                foreach (var claim in principal.Claims)
+                {
+                    claim.SetDestinations(GetDestinations(claim));
+                }
+
+                return SignIn(principal, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
             }
 
             throw new NotImplementedException("The specified grant type is not implemented.");
+        }
+
+        private IEnumerable<string> GetDestinations(Claim claim)
+        {
+            // Note: by default, claims are NOT automatically included in the access and identity tokens.
+            // To allow OpenIddict to serialize them, you must attach them a destination, that specifies
+            // whether they should be included in access tokens, in identity tokens or in both.
+
+            return claim.Type switch
+            {
+                Claims.Name or 
+                Claims.Subject 
+                    => ImmutableArray.Create(Destinations.AccessToken, Destinations.IdentityToken),
+
+                _ => ImmutableArray.Create(Destinations.AccessToken),
+            };
         }
     }
 }
