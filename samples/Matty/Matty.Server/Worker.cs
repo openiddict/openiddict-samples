9876ae20144948cc4c8dd19d482d3be63688950e
Mohamed Seada <mohamed.seada.1994@gmail.com>
2021-09-01 21:12:45 +0200

    Add a device code flow sample

---
+++ samples/Matty/Matty.Server/Worker.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OpenIddict.Abstractions;
+using Matty.Server.Data;
+using static OpenIddict.Abstractions.OpenIddictConstants;
+
+namespace Matty.Server
+{
+    public class Worker : IHostedService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public Worker(IServiceProvider serviceProvider)
+            => _serviceProvider = serviceProvider;
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using var scope = _serviceProvider.CreateScope();
+
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            await context.Database.EnsureCreatedAsync();
+
+            var manager = scope.ServiceProvider.GetRequiredService<IOpenIddictApplicationManager>();
+
+            if (await manager.FindByClientIdAsync("device") == null)
+            {
+                await manager.CreateAsync(new OpenIddictApplicationDescriptor
+                {
+                    ClientId = "device",
+                    Type = ClientTypes.Public,
+                    ConsentType = ConsentTypes.Explicit,
+                    DisplayName = "Device client",
+                    Permissions =
+                    {
+                        Permissions.GrantTypes.DeviceCode,
+                        Permissions.GrantTypes.RefreshToken,
+                        Permissions.Endpoints.Device,
+                        Permissions.Endpoints.Token,
+                        Permissions.Scopes.Email,
+                        Permissions.Scopes.Profile,
+                        Permissions.Scopes.Roles,
+                    }
+                });
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+}
