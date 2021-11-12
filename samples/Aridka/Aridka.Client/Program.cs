035fca4fc1b0dd0abdd07c74d9ff314e3419a23e
Kévin Chalet <kevinchalet@gmail.com>
2021-11-12 01:55:54 +0100

    Simplify the Aridka/Hollastin/Kalarba/Matty clients

--- samples/Aridka/Aridka.Client/Program.cs
+++ samples/Aridka/Aridka.Client/Program.cs
@@ -7,68 +7,58 @@ using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
+using var client = new HttpClient();
 
-namespace Aridka.Client
+try
 {
-    public class Program
+    var token = await GetTokenAsync(client);
+    Console.WriteLine("Access token: {0}", token);
+    Console.WriteLine();
+
+    var resource = await GetResourceAsync(client, token);
+    Console.WriteLine("API response: {0}", resource);
+    Console.ReadLine();
+}
+catch (HttpRequestException exception)
+{
+    var builder = new StringBuilder();
+    builder.AppendLine("+++++++++++++++++++++");
+    builder.AppendLine(exception.Message);
+    builder.AppendLine(exception.InnerException?.Message);
+    builder.AppendLine("Make sure you started the authorization server.");
+    builder.AppendLine("+++++++++++++++++++++");
+    Console.WriteLine(builder.ToString());
+}
+
+static async Task<string> GetTokenAsync(HttpClient client)
+{
+    var request = new HttpRequestMessage(HttpMethod.Post, "https://localhost:44385/connect/token");
+    request.Content = new FormUrlEncodedContent(new Dictionary<string, string>
     {
-        public static async Task Main(string[] args)
-        {
-            using var client = new HttpClient();
+        ["grant_type"] = "client_credentials",
+        ["client_id"] = "console",
+        ["client_secret"] = "388D45FA-B36B-4988-BA59-B187D329C207"
+    });
 
-            try
-            {
-                var token = await GetTokenAsync(client);
-                Console.WriteLine("Access token: {0}", token);
-                Console.WriteLine();
+    var response = await client.SendAsync(request, HttpCompletionOption.ResponseContentRead);
 
-                var resource = await GetResourceAsync(client, token);
-                Console.WriteLine("API response: {0}", resource);
-                Console.ReadLine();
-            }
-            catch (HttpRequestException exception)
-            {
-                var builder = new StringBuilder();
-                builder.AppendLine("+++++++++++++++++++++");
-                builder.AppendLine(exception.Message);
-                builder.AppendLine(exception.InnerException?.Message);
-                builder.AppendLine("Make sure you started the authorization server.");
-                builder.AppendLine("+++++++++++++++++++++");
-                Console.WriteLine(builder.ToString());
-            }
-        }
+    var payload = await response.Content.ReadFromJsonAsync<OpenIddictResponse>();
 
-        public static async Task<string> GetTokenAsync(HttpClient client)
-        {
-            var request = new HttpRequestMessage(HttpMethod.Post, "https://localhost:44385/connect/token");
-            request.Content = new FormUrlEncodedContent(new Dictionary<string, string>
-            {
-                ["grant_type"] = "client_credentials",
-                ["client_id"] = "console",
-                ["client_secret"] = "388D45FA-B36B-4988-BA59-B187D329C207"
-            });
-
-            var response = await client.SendAsync(request, HttpCompletionOption.ResponseContentRead);
-
-            var payload = await response.Content.ReadFromJsonAsync<OpenIddictResponse>();
-
-            if (!string.IsNullOrEmpty(payload.Error))
-            {
-                throw new InvalidOperationException("An error occurred while retrieving an access token.");
-            }
-
-            return payload.AccessToken;
-        }
-
-        public static async Task<string> GetResourceAsync(HttpClient client, string token)
-        {
-            var request = new HttpRequestMessage(HttpMethod.Get, "https://localhost:44385/api/message");
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
-
-            var response = await client.SendAsync(request, HttpCompletionOption.ResponseContentRead);
-            response.EnsureSuccessStatusCode();
-
-            return await response.Content.ReadAsStringAsync();
-        }
+    if (!string.IsNullOrEmpty(payload.Error))
+    {
+        throw new InvalidOperationException("An error occurred while retrieving an access token.");
     }
+
+    return payload.AccessToken;
+}
+
+static async Task<string> GetResourceAsync(HttpClient client, string token)
+{
+    var request = new HttpRequestMessage(HttpMethod.Get, "https://localhost:44385/api/message");
+    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+    var response = await client.SendAsync(request, HttpCompletionOption.ResponseContentRead);
+    response.EnsureSuccessStatusCode();
+
+    return await response.Content.ReadAsStringAsync();
 }
--- samples/Kalarba/Kalarba.Client/Program.cs
+++ samples/Kalarba/Kalarba.Client/Program.cs
@@ -6,55 +6,46 @@ using System.Net.Http.Json;
 using System.Threading.Tasks;
 using OpenIddict.Abstractions;
 
-namespace Kalarba.Client
+using var client = new HttpClient();
+
+var token = await GetTokenAsync(client, "alice@wonderland.com", "P@ssw0rd");
+Console.WriteLine("Access token: {0}", token);
+Console.WriteLine();
+
+var resource = await GetResourceAsync(client, token);
+Console.WriteLine("API response: {0}", resource);
+
+Console.ReadLine();
+
+static async Task<string> GetTokenAsync(HttpClient client, string email, string password)
 {
-    public class Program
+    var request = new HttpRequestMessage(HttpMethod.Post, "http://localhost:58779/connect/token");
+    request.Content = new FormUrlEncodedContent(new Dictionary<string, string>
     {
-        public static async Task Main(string[] args)
-        {
-            using var client = new HttpClient();
+        ["grant_type"] = "password",
+        ["username"] = email,
+        ["password"] = password
+    });
 
-            var token = await GetTokenAsync(client, "alice@wonderland.com", "P@ssw0rd");
-            Console.WriteLine("Access token: {0}", token);
-            Console.WriteLine();
+    var response = await client.SendAsync(request, HttpCompletionOption.ResponseContentRead);
 
-            var resource = await GetResourceAsync(client, token);
-            Console.WriteLine("API response: {0}", resource);
+    var payload = await response.Content.ReadFromJsonAsync<OpenIddictResponse>();
 
-            Console.ReadLine();
-        }
-
-        public static async Task<string> GetTokenAsync(HttpClient client, string email, string password)
-        {
-            var request = new HttpRequestMessage(HttpMethod.Post, "http://localhost:58779/connect/token");
-            request.Content = new FormUrlEncodedContent(new Dictionary<string, string>
-            {
-                ["grant_type"] = "password",
-                ["username"] = email,
-                ["password"] = password
-            });
-
-            var response = await client.SendAsync(request, HttpCompletionOption.ResponseContentRead);
-
-            var payload = await response.Content.ReadFromJsonAsync<OpenIddictResponse>();
-
-            if (!string.IsNullOrEmpty(payload.Error))
-            {
-                throw new InvalidOperationException("An error occurred while retrieving an access token.");
-            }
-
-            return payload.AccessToken;
-        }
-
-        public static async Task<string> GetResourceAsync(HttpClient client, string token)
-        {
-            var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost:58779/api/message");
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
-
-            var response = await client.SendAsync(request, HttpCompletionOption.ResponseContentRead);
-            response.EnsureSuccessStatusCode();
-
-            return await response.Content.ReadAsStringAsync();
-        }
+    if (!string.IsNullOrEmpty(payload.Error))
+    {
+        throw new InvalidOperationException("An error occurred while retrieving an access token.");
     }
+
+    return payload.AccessToken;
+}
+
+static async Task<string> GetResourceAsync(HttpClient client, string token)
+{
+    var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost:58779/api/message");
+    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+    var response = await client.SendAsync(request, HttpCompletionOption.ResponseContentRead);
+    response.EnsureSuccessStatusCode();
+
+    return await response.Content.ReadAsStringAsync();
 }
