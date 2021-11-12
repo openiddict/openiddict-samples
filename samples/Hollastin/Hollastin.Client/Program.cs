e21d705345825bebb97888a1c1239215cc88960c
Kévin Chalet <kevinchalet@gmail.com>
2021-11-12 01:55:54 +0100

    Simplify the Aridka/Hollastin/Kalarba/Matty clients

--- samples/Hollastin/Hollastin.Client/Program.cs
+++ samples/Hollastin/Hollastin.Client/Program.cs
@@ -7,72 +7,63 @@ using System.Net.Http.Json;
 using System.Threading.Tasks;
 using OpenIddict.Abstractions;
 
-namespace Hollastin.Client
+using var client = new HttpClient();
+
+const string email = "bob@le-magnifique.com", password = "}s>EWG@f4g;_v7nB";
+
+await CreateAccountAsync(client, email, password);
+
+var token = await GetTokenAsync(client, email, password);
+Console.WriteLine("Access token: {0}", token);
+Console.WriteLine();
+
+var resource = await GetResourceAsync(client, token);
+Console.WriteLine("API response: {0}", resource);
+
+Console.ReadLine();
+
+static async Task CreateAccountAsync(HttpClient client, string email, string password)
 {
-    public class Program
+    var response = await client.PostAsJsonAsync("https://localhost:44360/Account/Register", new { email, password });
+
+    // Ignore 409 responses, as they indicate that the account already exists.
+    if (response.StatusCode == HttpStatusCode.Conflict)
     {
-        public static async Task Main(string[] args)
-        {
-            using var client = new HttpClient();
-
-            const string email = "bob@le-magnifique.com", password = "}s>EWG@f4g;_v7nB";
-
-            await CreateAccountAsync(client, email, password);
-
-            var token = await GetTokenAsync(client, email, password);
-            Console.WriteLine("Access token: {0}", token);
-            Console.WriteLine();
-
-            var resource = await GetResourceAsync(client, token);
-            Console.WriteLine("API response: {0}", resource);
-
-            Console.ReadLine();
-        }
-
-        public static async Task CreateAccountAsync(HttpClient client, string email, string password)
-        {
-            var response = await client.PostAsJsonAsync("https://localhost:44360/Account/Register", new { email, password });
-
-            // Ignore 409 responses, as they indicate that the account already exists.
-            if (response.StatusCode == HttpStatusCode.Conflict)
-            {
-                return;
-            }
-
-            response.EnsureSuccessStatusCode();
-        }
-
-        public static async Task<string> GetTokenAsync(HttpClient client, string email, string password)
-        {
-            var request = new HttpRequestMessage(HttpMethod.Post, "https://localhost:44360/connect/token");
-            request.Content = new FormUrlEncodedContent(new Dictionary<string, string>
-            {
-                ["grant_type"] = "password",
-                ["username"] = email,
-                ["password"] = password
-            });
-
-            var response = await client.SendAsync(request, HttpCompletionOption.ResponseContentRead);
-
-            var payload = await response.Content.ReadFromJsonAsync<OpenIddictResponse>();
-
-            if (!string.IsNullOrEmpty(payload.Error))
-            {
-                throw new InvalidOperationException("An error occurred while retrieving an access token.");
-            }
-
-            return payload.AccessToken;
-        }
-
-        public static async Task<string> GetResourceAsync(HttpClient client, string token)
-        {
-            var request = new HttpRequestMessage(HttpMethod.Get, "https://localhost:44360/api/message");
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
-
-            var response = await client.SendAsync(request, HttpCompletionOption.ResponseContentRead);
-            response.EnsureSuccessStatusCode();
-
-            return await response.Content.ReadAsStringAsync();
-        }
+        return;
     }
+
+    response.EnsureSuccessStatusCode();
+}
+
+static async Task<string> GetTokenAsync(HttpClient client, string email, string password)
+{
+    var request = new HttpRequestMessage(HttpMethod.Post, "https://localhost:44360/connect/token");
+    request.Content = new FormUrlEncodedContent(new Dictionary<string, string>
+    {
+        ["grant_type"] = "password",
+        ["username"] = email,
+        ["password"] = password
+    });
+
+    var response = await client.SendAsync(request, HttpCompletionOption.ResponseContentRead);
+
+    var payload = await response.Content.ReadFromJsonAsync<OpenIddictResponse>();
+
+    if (!string.IsNullOrEmpty(payload.Error))
+    {
+        throw new InvalidOperationException("An error occurred while retrieving an access token.");
+    }
+
+    return payload.AccessToken;
+}
+
+static async Task<string> GetResourceAsync(HttpClient client, string token)
+{
+    var request = new HttpRequestMessage(HttpMethod.Get, "https://localhost:44360/api/message");
+    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+    var response = await client.SendAsync(request, HttpCompletionOption.ResponseContentRead);
+    response.EnsureSuccessStatusCode();
+
+    return await response.Content.ReadAsStringAsync();
 }
