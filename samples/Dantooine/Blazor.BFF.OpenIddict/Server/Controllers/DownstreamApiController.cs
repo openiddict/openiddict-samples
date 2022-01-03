a987e3357a4afa3a652d0eacb5feed128c44c3a0
damienbod <damien_bod@hotmail.com>
2022-01-03 21:36:43 +0100

    Adding a downstream API

---
+++ samples/Dantooine/Blazor.BFF.OpenIddict/Server/Controllers/DownstreamApiController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blazor.BFF.OpenIddict.Server.Controllers
+{
+    [ValidateAntiForgeryToken]
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DownstreamApiController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new List<string> { "some data", "more data", "loads of data" };
+        }
+    }
+}
