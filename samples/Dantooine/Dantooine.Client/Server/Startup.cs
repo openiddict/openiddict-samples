3a306f64e16a926d2e7992552de2d61afaf1c640
damienbod <damien_bod@hotmail.com>
2022-01-06 23:33:13 +0100

    remove file

--- samples/Dantooine/Dantooine.Client/Server/Startup.cs
+++ samples/Dantooine/Dantooine.Client/Server/Startup.cs
@@ -76,9 +76,7 @@ namespace Dantooine.BFF.Server
                 app.UseExceptionHandler("/Error");
             }
 
-            app.UseSecurityHeaders(
-                SecurityHeadersDefinitions.GetHeaderPolicyCollection(env.IsDevelopment(),
-                    "https://localhost:44319"));
+            app.UseSecurityHeaders(GetSecurityHeaderPolicy(env.IsDevelopment(),  "https://localhost:44319"));
 
             app.UseHttpsRedirection();
             app.UseBlazorFrameworkFiles();
@@ -109,6 +107,68 @@ namespace Dantooine.BFF.Server
 
                 endpoints.MapFallbackToPage("/_Host");
             });
+        }
+
+        public static HeaderPolicyCollection GetSecurityHeaderPolicy(bool isDev, string idpHost)
+        {
+            var policy = new HeaderPolicyCollection()
+                .AddFrameOptionsDeny()
+                .AddXssProtectionBlock()
+                .AddContentTypeOptionsNoSniff()
+                .AddReferrerPolicyStrictOriginWhenCrossOrigin()
+                .AddCrossOriginOpenerPolicy(builder =>
+                {
+                    builder.SameOrigin();
+                })
+                .AddCrossOriginResourcePolicy(builder =>
+                {
+                    builder.SameOrigin();
+                })
+                .AddCrossOriginEmbedderPolicy(builder =>
+                {
+                    builder.RequireCorp();
+                })
+                .AddContentSecurityPolicy(builder =>
+                {
+                    builder.AddObjectSrc().None();
+                    builder.AddBlockAllMixedContent();
+                    builder.AddImgSrc().Self().From("data:");
+                    builder.AddFormAction().Self().From(idpHost);
+                    builder.AddFontSrc().Self();
+                    builder.AddStyleSrc().Self();
+                    builder.AddBaseUri().Self();
+                    builder.AddFrameAncestors().None();
+
+                    builder.AddScriptSrc()
+                        .Self()
+                        .WithHash256("v8v3RKRPmN4odZ1CWM5gw80QKPCCWMcpNeOmimNL2AA=")
+                        .UnsafeEval();
+                })
+                .RemoveServerHeader()
+                .AddPermissionsPolicy(builder =>
+                {
+                    builder.AddAccelerometer().None();
+                    builder.AddAutoplay().None();
+                    builder.AddCamera().None();
+                    builder.AddEncryptedMedia().None();
+                    builder.AddFullscreen().All();
+                    builder.AddGeolocation().None();
+                    builder.AddGyroscope().None();
+                    builder.AddMagnetometer().None();
+                    builder.AddMicrophone().None();
+                    builder.AddMidi().None();
+                    builder.AddPayment().None();
+                    builder.AddPictureInPicture().None();
+                    builder.AddSyncXHR().None();
+                    builder.AddUsb().None();
+                });
+
+            if (!isDev)
+            {
+                policy.AddStrictTransportSecurityMaxAgeIncludeSubDomains(maxAgeInSeconds: 60 * 60 * 24 * 365);
+            }
+
+            return policy;
         }
     }
 }
