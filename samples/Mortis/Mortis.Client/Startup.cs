a6355dcfbd2a5201b4a0e14349705f0b66ad908a
Kévin Chalet <kevinchalet@gmail.com>
2022-02-17 19:20:07 +0100

    Add an ASP.NET MVC 5.2 sample

---
+++ samples/Mortis/Mortis.Client/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace Mortis.Client
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
