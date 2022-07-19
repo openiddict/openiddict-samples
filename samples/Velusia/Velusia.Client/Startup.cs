455d7d77a3e1891279dfd496cf5bfb3a31b69f16
Kévin Chalet <kevinchalet@gmail.com>
2022-07-19 17:09:05 +0200

    Update the Velusia and Mortis clients to use the OpenIddict client stack

--- samples/Velusia/Velusia.Client/Startup.cs
+++ samples/Velusia/Velusia.Client/Startup.cs
@@ -1,16 +1,36 @@
 using System;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using OpenIddict.Client;
+using Quartz;
+using Velusia.Client.Models;
+using static OpenIddict.Abstractions.OpenIddictConstants;
 
 namespace Velusia.Client;
 
 public class Startup
 {
+    public Startup(IConfiguration configuration)
+        => Configuration = configuration;
+
+    public IConfiguration Configuration { get; }
+
     public void ConfigureServices(IServiceCollection services)
     {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            // Configure the context to use Microsoft SQL Server.
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+
+            // Register the entity sets needed by OpenIddict.
+            // Note: use the generic overload if you need
+            // to replace the default OpenIddict entities.
+            options.UseOpenIddict();
+        });
+
         services.AddAuthentication(options =>
         {
             options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
@@ -19,43 +39,85 @@ public class Startup
         .AddCookie(options =>
         {
             options.LoginPath = "/login";
+            options.LogoutPath = "/logout";
             options.ExpireTimeSpan = TimeSpan.FromMinutes(50);
             options.SlidingExpiration = false;
-        })
-
-        .AddOpenIdConnect(options =>
-        {
-            // Note: these settings must match the application details
-            // inserted in the database at the server level.
-            options.ClientId = "mvc";
-            options.ClientSecret = "901564A5-E7FE-42CB-B10D-61EF6A8F3654";
-
-            options.RequireHttpsMetadata = false;
-            options.GetClaimsFromUserInfoEndpoint = true;
-            options.SaveTokens = true;
-
-            // Use the authorization code flow.
-            options.ResponseType = OpenIdConnectResponseType.Code;
-            options.AuthenticationMethod = OpenIdConnectRedirectBehavior.RedirectGet;
-
-            // Note: setting the Authority allows the OIDC client middleware to automatically
-            // retrieve the identity provider's configuration and spare you from setting
-            // the different endpoints URIs or the token validation parameters explicitly.
-            options.Authority = "https://localhost:44313/";
-
-            options.Scope.Add("email");
-            options.Scope.Add("roles");
-
-            // Disable the built-in JWT claims mapping feature.
-            options.MapInboundClaims = false;
-
-            options.TokenValidationParameters.NameClaimType = "name";
-            options.TokenValidationParameters.RoleClaimType = "role";
         });
+
+        // OpenIddict offers native integration with Quartz.NET to perform scheduled tasks
+        // (like pruning orphaned authorizations from the database) at regular intervals.
+        services.AddQuartz(options =>
+        {
+            options.UseMicrosoftDependencyInjectionJobFactory();
+            options.UseSimpleTypeLoader();
+            options.UseInMemoryStore();
+        });
+
+        // Register the Quartz.NET service and configure it to block shutdown until jobs are complete.
+        services.AddQuartzHostedService(options => options.WaitForJobsToComplete = true);
+
+        services.AddOpenIddict()
+
+            // Register the OpenIddict core components.
+            .AddCore(options =>
+            {
+                // Configure OpenIddict to use the Entity Framework Core stores and models.
+                // Note: call ReplaceDefaultEntities() to replace the default OpenIddict entities.
+                options.UseEntityFrameworkCore()
+                       .UseDbContext<ApplicationDbContext>();
+
+                // Developers who prefer using MongoDB can remove the previous lines
+                // and configure OpenIddict to use the specified MongoDB database:
+                // options.UseMongoDb()
+                //        .UseDatabase(new MongoClient().GetDatabase("openiddict"));
+
+                // Enable Quartz.NET integration.
+                options.UseQuartz();
+            })
+
+            // Register the OpenIddict client components.
+            .AddClient(options =>
+            {
+                // Enable the redirection endpoint needed to handle the callback stage.
+                //
+                // Note: to mitigate mix-up attacks, it's recommended to use a unique redirection endpoint
+                // address per provider, unless all the registered providers support returning an "iss"
+                // parameter containing their URL as part of authorization responses. For more information,
+                // see https://datatracker.ietf.org/doc/html/draft-ietf-oauth-security-topics#section-4.4.
+                options.SetRedirectionEndpointUris("/signin-local");
+
+                // Register the signing and encryption credentials used to protect
+                // sensitive data like the state tokens produced by OpenIddict.
+                options.AddDevelopmentEncryptionCertificate()
+                       .AddDevelopmentSigningCertificate();
+
+                // Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
+                options.UseAspNetCore()
+                       .EnableStatusCodePagesIntegration()
+                       .EnableRedirectionEndpointPassthrough();
+
+                // Register the System.Net.Http integration.
+                options.UseSystemNetHttp();
+
+                // Add a client registration matching the client application definition in the server project.
+                options.AddRegistration(new OpenIddictClientRegistration
+                {
+                    Issuer = new Uri("https://localhost:44313/", UriKind.Absolute),
+
+                    ClientId = "mvc",
+                    ClientSecret = "901564A5-E7FE-42CB-B10D-61EF6A8F3654",
+                    RedirectUri = new Uri("https://localhost:44338/signin-local", UriKind.Absolute),
+                    Scopes = { Scopes.Email, Scopes.Profile }
+                });
+            });
+
+        services.AddHttpClient();
 
         services.AddControllersWithViews();
 
-        services.AddHttpClient();
+        // Register the worker responsible for creating the database used to store tokens.
+        // Note: in a real world application, this step should be part of a setup script.
+        services.AddHostedService<Worker>();
     }
 
     public void Configure(IApplicationBuilder app)
---
+++ samples/Velusia/Velusia.Client/Worker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Velusia.Client.Models;
+
+namespace Velusia.Client;
+
+public class Worker : IHostedService
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public Worker(IServiceProvider serviceProvider)
+        => _serviceProvider = serviceProvider;
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await using var scope = _serviceProvider.CreateAsyncScope();
+
+        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        await context.Database.EnsureCreatedAsync(cancellationToken);
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+}
