b6f5a07506f624e41bdc99bd5eacf472b4123e02
Kévin Chalet <kevinchalet@gmail.com>
2022-12-09 10:30:52 +0100

    Enable authorization code flow support in the Mortis.Client sample

--- samples/Hollastin/Hollastin.Client/Program.cs
+++ samples/Hollastin/Hollastin.Client/Program.cs
@@ -8,15 +8,26 @@ using Microsoft.Extensions.DependencyInjection;
 using OpenIddict.Client;
 
 var services = new ServiceCollection();
+
 services.AddOpenIddict()
+
+    // Register the OpenIddict client components.
     .AddClient(options =>
     {
+        // Allow grant_type=password to be negotiated.
         options.AllowPasswordFlow();
 
+        // Disable token storage, which is not necessary for non-interactive flows like
+        // grant_type=password, grant_type=client_credentials or grant_type=refresh_token.
         options.DisableTokenStorage();
 
-        options.UseSystemNetHttp();
+        // Register the System.Net.Http integration and use the identity of the current
+        // assembly as a more specific user agent, which can be useful when dealing with
+        // providers that use the user agent as a way to throttle requests (e.g Reddit).
+        options.UseSystemNetHttp()
+               .SetProductInformation(typeof(Program).Assembly);
 
+        // Add a client registration without a client identifier/secret attached.
         options.AddRegistration(new OpenIddictClientRegistration
         {
             Issuer = new Uri("https://localhost:44360/", UriKind.Absolute)
--- samples/Imynusoph/Imynusoph.Client/Program.cs
+++ samples/Imynusoph/Imynusoph.Client/Program.cs
@@ -8,16 +8,27 @@ using OpenIddict.Client;
 using static OpenIddict.Abstractions.OpenIddictConstants;
 
 var services = new ServiceCollection();
+
 services.AddOpenIddict()
+
+    // Register the OpenIddict client components.
     .AddClient(options =>
     {
+        // Allow grant_type=password and grant_type=refresh_token to be negotiated.
         options.AllowPasswordFlow()
                .AllowRefreshTokenFlow();
 
+        // Disable token storage, which is not necessary for non-interactive flows like
+        // grant_type=password, grant_type=client_credentials or grant_type=refresh_token.
         options.DisableTokenStorage();
 
-        options.UseSystemNetHttp();
+        // Register the System.Net.Http integration and use the identity of the current
+        // assembly as a more specific user agent, which can be useful when dealing with
+        // providers that use the user agent as a way to throttle requests (e.g Reddit).
+        options.UseSystemNetHttp()
+               .SetProductInformation(typeof(Program).Assembly);
 
+        // Add a client registration without a client identifier/secret attached.
         options.AddRegistration(new OpenIddictClientRegistration
         {
             Issuer = new Uri("https://localhost:44382/", UriKind.Absolute)
