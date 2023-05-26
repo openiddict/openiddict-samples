389def2c1a63e1bf98acb2b41b66836ddd7eead2
greendimka <2776617+greendimka@users.noreply.github.com>
2023-05-26 18:57:17 +0200

    Add a comment in Imynusoph.Client indicating that the "offline_access" scope is required to retrieve a refresh token

--- samples/Imynusoph/Imynusoph.Client/Program.cs
+++ samples/Imynusoph/Imynusoph.Client/Program.cs
@@ -75,6 +75,7 @@ static async Task<(string AccessToken, string RefreshToken)> GetTokensAsync(ISer
 {
     var service = provider.GetRequiredService<OpenIddictClientService>();
 
+    // Note: the "offline_access" scope must be requested and granted to receive a refresh token.
     var (response, _) = await service.AuthenticateWithPasswordAsync(
         issuer  : new Uri("https://localhost:44382/", UriKind.Absolute),
         scopes  : new[] { Scopes.OfflineAccess },
