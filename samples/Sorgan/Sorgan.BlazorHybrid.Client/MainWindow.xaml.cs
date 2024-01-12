46a1c88fe3938f9cef08b141ca048c746cf08326
Kévin Chalet <kevinchalet@gmail.com>
2024-01-12 10:41:24 +0100

    Add a Blazor Hybrid sample using GitHub authentication

---
+++ samples/Sorgan/Sorgan.BlazorHybrid.Client/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;
+using Dapplo.Microsoft.Extensions.Hosting.Wpf;
+
+namespace Sorgan.BlazorHybrid.Client;
+
+public partial class MainWindow : Window, IWpfShell
+{
+    public MainWindow(IServiceProvider provider)
+    {
+        InitializeComponent();
+
+        Resources.Add("services", provider);
+    }
+}
