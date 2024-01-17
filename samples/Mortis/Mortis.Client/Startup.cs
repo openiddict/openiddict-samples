c12ca77a0d131d9d9662bb7ce1c2dd9d62fb19e9
Kévin Chalet <kevinchalet@gmail.com>
2024-01-17 23:40:32 +0100

    Remove the manual claims mapping logic from the Mortis.Client sample

--- samples/Mortis/Mortis.Client/Startup.cs
+++ samples/Mortis/Mortis.Client/Startup.cs
@@ -64,6 +64,8 @@ namespace Mortis.Client
                     options.AddRegistration(new OpenIddictClientRegistration
                     {
                         Issuer = new Uri("https://localhost:44349/", UriKind.Absolute),
+                        ProviderName = "Local",
+                        ProviderDisplayName = "Local OIDC server",
 
                         ClientId = "mvc",
                         ClientSecret = "901564A5-E7FE-42CB-B10D-61EF6A8F3654",
