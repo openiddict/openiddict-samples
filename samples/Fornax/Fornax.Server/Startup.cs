1bc414e9e04092a0ea8d575d88f59c8e2835e524
Kévin Chalet <kevinchalet@gmail.com>
2024-02-20 17:43:55 +0100

    Remove unnecessary NuGet dependencies from the ASP.NET 4.8 samples

--- samples/Fornax/Fornax.Server/Startup.cs
+++ samples/Fornax/Fornax.Server/Startup.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Owin;
+﻿using System;
+using Fornax.Server.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
 using OpenIddict.Server.Owin;
 using OpenIddict.Validation.Owin;
 using Owin;
@@ -7,11 +12,31 @@ using Owin;
 
 namespace Fornax.Server
 {
-    public partial class Startup
+    public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            // Register the Entity Framework context and the user/sign-in managers used by ASP.NET Identity.
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+
+            // Register the cookie middleware used by ASP.NET Identity.
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+                Provider = new CookieAuthenticationProvider
+                {
+                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
+                        validateInterval: TimeSpan.FromMinutes(30),
+                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+                }
+            });
+
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+            app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
+            app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
 
             // Register the Autofac scope injector middleware.
             app.UseAutofacLifetimeScopeInjector(Global.Provider.ApplicationContainer);
