3cf892f39d8f3225b886137f6e27bcfbfd9c3564
Manuel Menegazzo <manuel.menegazzo@outlook.com>
2026-01-29 20:52:16 +0100

    Removed trailing comma

--- samples/Balosar/Balosar.Server/Worker.cs
+++ samples/Balosar/Balosar.Server/Worker.cs
@@ -51,7 +51,7 @@ public class Worker : IHostedService
                 Requirements =
                 {
                     Requirements.Features.ProofKeyForCodeExchange
-                },
+                }
             });
         }
     }
