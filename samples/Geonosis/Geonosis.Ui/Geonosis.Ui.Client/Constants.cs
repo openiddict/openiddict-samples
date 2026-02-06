393481e1549bef30033e655e3a3ca67820016c1a
Manuel Menegazzo <manuel.menegazzo@outlook.com>
2026-02-06 12:10:30 +0100

    Added login/logout buttons

---
+++ samples/Geonosis/Geonosis.Ui/Geonosis.Ui.Client/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Geonosis.Ui.Client
+{
+    public static class Constants
+    {
+        public const string LoginPath = "/authentication/login";
+        public const string LogoutPath = "/authentication/logout";
+    }
+}
