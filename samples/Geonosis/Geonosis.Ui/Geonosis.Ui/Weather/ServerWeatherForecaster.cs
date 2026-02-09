2b620a274ec74e4859073dafd7f7e912b116664d
Manuel Menegazzo <manuel.menegazzo@outlook.com>
2026-02-09 19:53:14 +0100

    Updated UserClaims page

--- samples/Geonosis/Geonosis.Ui/Geonosis.Ui/Weather/ServerWeatherForecaster.cs
+++ samples/Geonosis/Geonosis.Ui/Geonosis.Ui/Weather/ServerWeatherForecaster.cs
@@ -12,25 +12,22 @@ namespace Geonosis.Ui.Weather
     internal sealed class ServerWeatherForecaster() : IWeatherForecaster
     //internal sealed class ServerWeatherForecaster(HttpClient httpClient, IHttpContextAccessor httpContextAccessor) : IWeatherForecaster
     {
-        public Task<IEnumerable<WeatherForecast>> GetWeatherForecastAsync()
+        public async Task<IEnumerable<WeatherForecast>> GetWeatherForecastAsync()
         {
-            // Retrieve the data stored by OpenIddict in the state token created when the logout was triggered.
-            //var result = await httpContextAccessor.HttpContext.User .AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            //// Retrieve the data stored by OpenIddict in the state token created when the logout was triggered.
+            ////var result = await httpContextAccessor.HttpContext.User .AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
 
-            //var openIddictClientService = transformContext.HttpContext.RequestServices.GetRequiredService<OpenIddictClientService>();
-            //var accessToken = await transformContext.HttpContext.GetTokenAsync(OpenIddictClientAspNetCoreConstants.Tokens.BackchannelAccessToken)
-            //    ?? throw new InvalidOperationException("The access token cannot be retrieved.");
+            ////var openIddictClientService = transformContext.HttpContext.RequestServices.GetRequiredService<OpenIddictClientService>();
+            ////var accessToken = await transformContext.HttpContext.GetTokenAsync(OpenIddictClientAspNetCoreConstants.Tokens.BackchannelAccessToken)
+            ////    ?? throw new InvalidOperationException("The access token cannot be retrieved.");
 
             //var exchangeResult = await openIddictClientService.AuthenticateWithTokenExchangeAsync(new()
             //{
             //    SubjectToken = accessToken,
             //    SubjectTokenType = TokenTypeIdentifiers.AccessToken,
             //    RequestedTokenType = TokenTypeIdentifiers.AccessToken,
-            //    Scopes = [Scopes.OfflineAccess, Scopes.Profile, "Weather.Read"],
+            //    Scopes = ["Weather.Read"],
             //});
-
-            //var httpContext = httpContextAccessor.HttpContext ??
-            //    throw new InvalidOperationException("No HttpContext available from the IHttpContextAccessor!");
 
             //using var request = new HttpRequestMessage(HttpMethod.Get, "/weather-forecast");
             //request.Headers.Authorization = new("Bearer", exchangeResult.IssuedToken);
@@ -38,10 +35,8 @@ namespace Geonosis.Ui.Weather
             //using var response = await httpClient.SendAsync(request);
             //response.EnsureSuccessStatusCode();
 
-            //return await response.Content.ReadFromJsonAsync<WeatherForecast[]>() ??
-            //    throw new IOException("No weather forecast!");
-
-            return Task.FromResult(Enumerable.Empty<WeatherForecast>());
+            //return await response.Content.ReadFromJsonAsync<WeatherForecast[]>() ?? [];
+            throw new NotImplementedException("This method is not implemented yet.");
         }
     }
 }
